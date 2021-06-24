ba6ffeecb1eb62e3010d1e92cb76e958927baf68
Ajurgs <ajjurgs5954@gmail.com>
2021-06-24 16:42:24 -0600

    filled out the resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,13 +1,29 @@
 import React from 'react';
-
+import pdf from '../../pdf/Resume.pdf'
 
 export default function Resume(){
     return(
-        <>
-        <section id="about" className="sectionHeader">
-            <h2 className="section">Resume</h2>
-            <div className="strike"></div>
+        <div className="container">
+        <section id="about" className="card border-light">
+            <h2 className="card-header bg-light">Resume</h2>
+            <div className="card-body">
+                <a href={pdf}>Download Resume</a>
+                <h3>Proficiencies</h3>
+                <ul className=''>
+                    <li>C#</li>
+                    <li>JavaScript</li>
+                    <li>Node.js</li>
+                    <li>express.js</li>
+                    <li>MySql</li>
+                    <li>MongoDB</li>
+                    <li>React</li>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>C++</li>
+                </ul>
+            </div>
+
         </section>
-        </>
+        </div>
     )
 }
